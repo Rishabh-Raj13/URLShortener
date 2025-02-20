95b7eade915442507e1f764ca53c718c2d488aeb
Rishabh Raj <rishabhraj1310@gmail.com>
2025-02-20 18:29:51 +0530

    Updated the shortening algo to use base64 and MD5 and addressed the collision issue

--- urlShortener/locustfile.py
+++ urlShortener/locustfile.py
@@ -1,13 +1,23 @@
-from locust import HttpUser,task,between
+from locust import HttpUser, task, between
 
-class DjangoLoadTest(HttpUser):
-    wait_time=between(1,3)
+class URLShortenerUser(HttpUser):
+    wait_time = between(1, 3)  # Wait between 1-3 seconds randomly
+
+    def on_start(self):
+        """Generate a short URL once when a user starts"""
+        response = self.client.post("urlShort/", json={"url": "https://www.youtube.com"})
+        if response.status_code == 201:  # Assuming 201 Created
+            self.short_url = response.json().get("slug")  # Adjust based on API response
+        else:
+            self.short_url = None
 
     @task
-    def home_page(self):
-        self.client.get("urlShort/")
+    def shorten_url(self):
+        """Test shortening a URL"""
+        self.client.post("urlShort/", json={"url": "https://www.youtube.com"})
 
     @task
-    def short_url_redirect(self):
-        self.client.get("http://127.0.0.1:8000/urlShort/SrUZSwDfEY")
-    
+    def access_shortened_url(self):
+        """Test accessing a shortened URL"""
+        if self.short_url:
+            self.client.get(self.short_url)
